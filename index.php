85c6cc084f905082ba7fa1dcdcd23ae6771780e6
Khanz <hani_rune@hotmail.com>
2018-03-27 11:27:08 +0200

    Finally added a main thing xD

--- index.php
+++ index.php
@@ -17,22 +17,41 @@
         <div class="quick-access">
             <div class="left cont">
                 <div class="big">
-
+                    <img src="images/mainphoto2.jpg" alt="">
+                    <a href="">Quests</a>
                 </div>
-                <div class="double"></div>
+                <div class="double">
+                    <div class="double-right">
+                        <img src="images/discord.jpg" alt="">
+                        <a href="">Discord</a>
+                    </div>
+                    <div class="double-left">
+
+                        <img src="images/reddit.png" alt="">
+                        <a href="">Reddit</a>
+
+                    </div>
+                </div>
             </div>
 
             <div class="right cont">
                 <div class="double">
+                    <div class="double-right">
+                        <img src="images/mainphoto3.jpg" alt="">
+                        <a href="">About Us</a>
+                    </div>
 
+                    <div class="double-left">
+                        <img src="images/mainphoto1.jpg" alt="">
+                        <a href="">Github</a>
+                    </div>
                 </div>
                 <div class="big">
-
+                    <img src="images/mainphoto4.jpg" alt="">
+                    <a href="">Article Of The Week</a>
                 </div>
             </div>
         </div>
-
-
 
     </main>
 
